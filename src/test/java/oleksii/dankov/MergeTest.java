e97d608c413ab339a57f8600020e96566e7eb7d3
denimkqqa <denimkqqa@gmail.com>
2019-01-31 12:13:38 +0200

    resources moved, added test dependencies, rewritten merge test

--- src/test/java/oleksii/dankov/MergeTest.java
+++ src/test/java/oleksii/dankov/MergeTest.java
@@ -2,50 +2,71 @@ package oleksii.dankov;
 
 import oleksii.dankov.cli.CliArgumentHandler;
 import oleksii.dankov.merger.ResourceMergerImpl;
-import oleksii.dankov.merger.ResourcesMergingException;
-import oleksii.dankov.writer.DocumentWriterException;
 import oleksii.dankov.writer.DocumentWriterImpl;
-import org.apache.commons.cli.ParseException;
+import org.junit.After;
 import org.junit.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.xmlmatchers.namespace.SimpleNamespaceContext;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
+import javax.xml.transform.Source;
 import java.io.File;
-import java.io.IOException;
-import java.util.Map;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Collection;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.xmlmatchers.XmlMatchers.equivalentTo;
+import static org.xmlmatchers.XmlMatchers.isEquivalentTo;
+import static org.xmlmatchers.XmlMatchers.similarTo;
+import static org.xmlmatchers.transform.XmlConverters.the;
+import static org.xmlmatchers.transform.XmlConverters.xml;
+import static org.xmlmatchers.xpath.HasXPath.hasXPath;
 
+import static org.xmlmatchers.xpath.XpathReturnType.returningAString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.xmlmatchers.xpath.XpathReturnType.returningAnXmlNode;
+
+@RunWith(Parameterized.class)
 public class MergeTest {
-    @Test
-    public void generalAppTest() throws DocumentWriterException, ResourcesMergingException, ParseException, ParserConfigurationException, SAXException, IOException {
+
+
+    private final String xmlData;
+
+    public MergeTest(String xml) {
+        this.xmlData = xml;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<String[]> data() throws Exception {
         String[] args = {
-                "-libsDir", "src/test/java/oleksii/dankov/libs",
-                "-appRes", "src/test/java/oleksii/dankov/appres/",
+                "-libsDir", "src/test/resources/libs",
+                "-appRes", "src/test/resources/appres/",
                 "-outputDirectory", "tmp/output"
         };
-        File original = new File("src/test/java/oleksii/dankov/expected_merged.xml");
         File generated = new File("tmp/output/values/values.xml");
         CliArgumentHandler argsHandler = CliArgumentHandler.fromArgs(args);
         new App(argsHandler, new DocumentWriterImpl(), new ResourceMergerImpl()).process();
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        NodeList nodesOriginal = parseFile(original, dbFactory);
-        NodeList nodesGenerated = parseFile(generated, dbFactory);
-        assertEquals(nodesOriginal.getLength(), nodesGenerated.getLength());
+        String xml = new String(Files.readAllBytes(generated.toPath()));
+        return Arrays.asList(new String[][] {//
+                { xml },
+        });
     }
-    private static NodeList parseFile(File original, DocumentBuilderFactory dbFactory) throws SAXException, IOException, ParserConfigurationException {
-        DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
 
-        Document doc = dbBuilder.parse(original);
-        Element rootElement = doc.getDocumentElement();
-        rootElement.normalize();
-        return rootElement.getChildNodes();
+    @Test
+    public void hasAllFields() {
+        Source xml = the(xmlData);
+        assertThat(xml, hasXPath("/resources/string[@name='app_name']"));
+        assertThat(xml, hasXPath("/resources/color[@name='colorAccent']"));
+        assertThat(xml, hasXPath("/resources/color[@name='colorPrimary']"));
+        assertThat(xml, hasXPath("/resources/color[@name='colorPrimaryDark']"));
+    }
+
+    @Test
+    public void resourcesHasNs1NameSpace() {
+        assertTrue(xmlData.contains("xmlns:ns1=\"urn:oasis:names:tc:xliff:document:1.2\""));
     }
 }
